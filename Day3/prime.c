7a3d1d167396f071f6b1194350b46ecfad43eec7
anshuman <anshumanbajpai7270@gmail.com>
2026-06-05 17:27:31 +0530

    Day-3 corrected

--- Day3/prime.c
+++ Day3/prime.c
@@ -5,13 +5,7 @@
   printf("\n ENTER A NUMBER ; ");
   scanf("%d",&num);
   
- if(prime(num)){
-    printf("\n%d IS A PRIME NUMBER",num);
- }
- else
- {
-    printf("\n%d IS NOT A PRIME NUMBER",num);
- }
+   prime(num);
  
     return 0;
  }
@@ -19,15 +13,20 @@
 
 int prime(int num)
 {
- int i;
- for(i=2;i<num;i++)
+ int i ,prime=1;
+ for(i=2;i*i<=num;i++)
  {
-  if(num%i!=0){
-    return 1;
-  }else 
-   {
-    return 0;
+  if(num%i==0){
+     prime =0; 
+     break;
   }
+}
+   if (prime==1){
+    printf("\n%d IS A PRIME NUMBER",num);
  }
-    
+   else
+    {
+      printf("\n%d IS NOT A PRIME NUMBER",num);
+    }
+    return 0;
 }
